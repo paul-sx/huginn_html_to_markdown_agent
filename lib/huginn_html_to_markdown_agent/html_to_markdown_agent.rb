ce457c2fffa2619163011a1dd6820e894990e46d
Paul <git@paul.sx>
2022-06-02 11:00:46 -0400

    Basic functionality

--- lib/huginn_html_to_markdown_agent/html_to_markdown_agent.rb
+++ lib/huginn_html_to_markdown_agent/html_to_markdown_agent.rb
@@ -1,24 +1,37 @@
+require 'reverse_markdown'
 module Agents
   class HtmlToMarkdownAgent < Agent
-    default_schedule '12h'
+    default_schedule 'never'
 
     description <<-MD
-      Add a Agent description here
+      The html to markdown agent takes input in the form of an html string and converts it to markdown.
+
+      Use the source option (with liquid formatting) to set the html string you want to convert.  
     MD
 
     def default_options
       {
-        source: "<html><body>Code</body></html>"
+        'source' => "<html><body>Code</body></html>"
       }
     end
 
     def validate_options
+      errors.add(:base, 'source_missing') unless options['source'].present?
+      errors.add(:base, 'source_not_string') unless options['source'] === String
     end
 
-#    def check
-#    end
+    def check
+      html_input = interpolated("source")
+      output = ReverseMarkdown.convert(html_input, unknown_tags: :bypass)
+      create_event payload: {'markdown' => output}
+    end
 
-#    def receive(incoming_events)
-#    end
+    def receive(incoming_events)
+      interpolate_with_each(incoming_events) do |event|
+        html_input = interpolated("source")
+        output = ReverseMarkdown.convert(html_input, unknown_tags: :bypass)
+        create_event payload: event.payload.merge({'markdown' => output})
+      end
+    end
   end
 end
